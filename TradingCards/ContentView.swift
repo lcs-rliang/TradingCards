85707dd5c101272b922df027a4326ab7973293f0
Ruiyan Liang <Jliang@lcs.on.ca>
2023-01-12 12:43:36 -0500

    Initial Commit

---
+++ TradingCards/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TradingCards
+//
+//  Created by dyhidrogen monoxide on 2023/1/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by dyhidrogen monoxide on 2023/1/12.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
